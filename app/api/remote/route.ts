69b3f3f2e92826769a7db3964e50c285979ab81f
Davide Ghiotto <davide.ghiotto@bitrock.it>
2025-04-17 22:12:37 +0200

    fix: handle database insertion errors and improve webhook response message

--- app/api/remote/route.ts
+++ app/api/remote/route.ts
@@ -1,3 +1,4 @@
+import { sql } from "@/app/(config)/postgres";
 import { NextResponse } from "next/server";
 import { computeReputationScoring } from "reputation-scoring";
 
@@ -19,10 +20,29 @@ export async function POST(request: Request) {
       token: process.env.GITHUB_TOKEN,
     });
 
+    try {
+      await sql(
+        `INSERT INTO public."Scores" (repository, user, score) VALUES ${scores
+          .map(
+            (score) =>
+              `('${payload.repository.name}', '${score.user}', ${score.score})`
+          )
+          .join(", ")}`
+      );
+    } catch (error) {
+      console.error("Database error:", error);
+      return NextResponse.json(
+        { error: "Internal server error" },
+        { status: 500 }
+      );
+    }
+
+    // Handle the webhook based on the event type
+
     // For now, we'll just log it and return a success response
     return NextResponse.json({
       success: true,
-      message: `Processed ${githubEvent} webhook for ${
+      message: `Updated ${githubEvent} webhook for ${
         payload.repository.name
       }: ${scores.map((score) => `${score.user}: ${score.score}`)}`,
     });
